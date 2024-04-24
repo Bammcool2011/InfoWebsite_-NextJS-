8a223000f36f931bff4f7b2acb91594bd4695ea1
Pumin Wangprasit <savee2011@hotmail.com>
2024-04-24 11:41:00 +0700

    add box-content, profile pic, some desc.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-  <Navbar/>
+        <Navbar />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 import Image from "next/image";
-import Navbar from "@/component/navbar";
 
 export default function Home() {
   return (
     <>
-    <div className="text-9xl flex justify-center">
-      Hello World
-    </div>
+      <section className=" flex justify-center">
+        <div className=" box-content bg-white w-[15rem] h-[15rem] rounded-full m-10"></div>
+      </section>
+
+      <section className=" flex justify-center">
+        <div className=" box-content w-[45rem] bg-gray-900 rounded-lg m-5 p-5">
+          <div className=" text-center">
+            <span className=" text-4xl leading-normal font-mono font-bold">
+              HELLO MY NAME IS PUMIN 👋
+              <br />
+              THIS IS MY FIRST PORTFOLIO WEBSITE
+              <br />
+            </span>
+            <span className=" text-lg font-mono font-bold">
+              (CREATED WITH NEXTJS)
+            </span>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
