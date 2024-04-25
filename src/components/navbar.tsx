b922c2bcf231f988f6f55c4db18c407844fc1b1b
Pumin Wangprasit <savee2011@hotmail.com>
2024-04-25 10:51:04 +0700

    make responsitive for navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,25 +1,82 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 
 export default function Navbar() {
+  const [isClick, setIsClick] = useState(false);
+  const toggleNavbar = (): void => {
+    setIsClick(!isClick);
+  };
+
   return (
-    <nav className="bg-gray-800 text-white p-4">
+    <nav className="bg-gray-800 text-white p-4 sm:p-4 md:flex md:justify-between md:items-center">
       <div className=" container mx-auto flex justify-between items-center">
         <a href="" className="text-2xl font-bold">
           Portfolio
         </a>
-        <div>
-          <Link href="/" className="mx-3">
+        <div className="hidden md:flex">
+          <Link href="/" className="mx-3 hover:text-gray-300">
             Home
           </Link>
-          <Link href="/works" className="mx-3">
+          <Link href="/works" className="mx-3 hover:text-gray-300">
             Works
           </Link>
-          <Link href="/contact" className="mx-3">
+          <Link href="/contact" className="mx-3 hover:text-gray-300">
             Contact
           </Link>
         </div>
+
+        <div className="md:hidden flex items-center">
+          <button onClick={toggleNavbar}>
+            {isClick ? (
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
       </div>
+      {isClick && (
+        <div className="md:hidden">
+          <div className="px-1 pt-2 pb-3 space-y-3 sm:px-2">
+            <Link href="/" className="mx-3 block hover:text-gray-300">
+              Home
+            </Link>
+            <Link href="/works" className="mx-3 block hover:text-gray-300">
+              Works
+            </Link>
+            <Link href="/contact" className="mx-3 block hover:text-gray-300">
+              Contact
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
